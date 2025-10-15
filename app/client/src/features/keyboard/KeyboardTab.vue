d3a8a54facdc09246913155b1f0c3c5682cba0a2
francis <francis.marasigan@gmail.com>
2025-10-15 16:03:59 -0700

    Added svg icon butttons.

--- app/client/src/features/keyboard/KeyboardTab.vue
+++ app/client/src/features/keyboard/KeyboardTab.vue
@@ -62,20 +62,28 @@
                 </td>
                 <td class="binding-actions">
                   <template v-if="captureActionId === action.id">
-                    <button class="btn-secondary" @click="cancelCapture">
-                      Cancel
+                    <button class="btn-icon" @click="cancelCapture" title="Cancel">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
+                        <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
+                      </svg>
                     </button>
                   </template>
                   <template v-else>
-                    <button class="btn" :disabled="!shortcutsEnabled" @click="startCapture(action.id)">
-                      Change
+                    <button class="btn-icon" :disabled="!shortcutsEnabled" @click="startCapture(action.id)" title="Change">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
+                        <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325"/>
+                      </svg>
                     </button>
                     <button
-                      class="btn-secondary"
+                      class="btn-icon btn-icon-danger"
                       :disabled="!shortcutsEnabled || !bindingMap[action.id]"
                       @click="clearBinding(action.id)"
+                      title="Remove"
                     >
-                      Remove
+                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
+                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+                        <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+                      </svg>
                     </button>
                   </template>
                 </td>
@@ -367,6 +375,35 @@ onBeforeUnmount(() => {
   padding: 6px 12px;
   color: inherit;
   cursor: pointer;
+}
+
+.btn-icon {
+  background: transparent;
+  border: 1px solid var(--color-border);
+  border-radius: var(--radius-small);
+  padding: 8px 16px;
+  color: var(--color-text-primary);
+  cursor: pointer;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  transition: all 0.2s ease;
+}
+
+.btn-icon:hover:not(:disabled) {
+  background: var(--color-surface-muted);
+  border-color: var(--color-accent);
+  color: var(--color-accent);
+}
+
+.btn-icon:disabled {
+  cursor: not-allowed;
+  opacity: 0.4;
+}
+
+.btn-icon-danger:hover:not(:disabled) {
+  border-color: var(--color-danger, #f87171);
+  color: var(--color-danger, #f87171);
 }
 
 .error {
