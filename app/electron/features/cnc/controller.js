fe4ecfd09fff0b42cecb89a1ae3dec3233e2fb5f
francis <francis.marasigan@gmail.com>
2026-01-25 12:51:28 -0800

    Increase jog watchdog timeout from 500ms to 1 second

--- app/electron/features/cnc/controller.js
+++ app/electron/features/cnc/controller.js
@@ -69,7 +69,7 @@ export class CNCController extends EventEmitter {
 
     // Dead-man switch for continuous jogging
     this.jogWatchdog = new JogWatchdog({
-      timeoutMs: 500,
+      timeoutMs: 1000,
       onTimeout: (reason) => this.sendEmergencyJogCancel(reason)
     });
 
