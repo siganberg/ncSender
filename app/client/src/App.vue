a15fefeb5210ad64aa24a90d4a8b9e00a98e0038
francis <francis.marasigan@gmail.com>
2026-02-06 10:19:49 -0800

    Change firmware settings export extension from .grbl to .txt

--- app/client/src/App.vue
+++ app/client/src/App.vue
@@ -2274,7 +2274,7 @@ const exportFirmwareSettings = () => {
   const url = URL.createObjectURL(blob);
   const a = document.createElement('a');
   a.href = url;
-  a.download = `firmware-settings-${new Date().toISOString().split('T')[0]}.grbl`;
+  a.download = `firmware-settings-${new Date().toISOString().split('T')[0]}.txt`;
   document.body.appendChild(a);
   a.click();
   document.body.removeChild(a);
